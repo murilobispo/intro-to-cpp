248bbe0b6e83891e2dc0473ace0e6213c08ef1eb
murilobispo <murilooliveirabispo90@gmail.com>
2025-11-27 22:39:58 -0300

    Idealizing quick sort

--- sort_algorithms.cpp
+++ sort_algorithms.cpp
@@ -20,12 +20,45 @@ void bubbleSort(int array[], int size){
         }
     }
 }
+void quickSort(int array[], int size){
+    
+    int pivot = array[0];
+    int r = size - 1;
+    int l = 0;
+    while(pivot < array[r]) r--;
+    array[0] = array[r];
+    array[r] = pivot;
+    while(pivot > l) l++;
+    array[r] = array[l];
+    array[l] = pivot;
+    
+    pivot = array[0];
+    r = l - 1;
+    l = 0;
+    while(pivot < array[r]) r--;
+    array[0] = array[r];
+    array[r] = pivot;
+    while(pivot > array[l]) l++;
+    array[r] = array[l];
+    array[l] = pivot;
+    
+    pivot = array[0];
+    r = l - 1;
+    l = 0;
+    while(pivot < array[r]) r--;
+    array[0] = array[r];
+    array[r] = pivot;
+    while(pivot > array[l]) l++;
+    array[r] = array[l];
+    array[l] = pivot;
+    
+}
 
 int main(){
-    int size = 5;
-    int array[size] = {7,10,5,6,8};
+    int size = 6;
+    int array[size] =  {3, 0, 1, 4, 2, 5};
     
-    bubbleSort(array, size);
+    quickSort(array, size);
     
     for(int i = 0; i < size; i++){
         cout << array[i] << " ";
